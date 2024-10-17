e9c558a205f9d7197d91cdcc09f6e36b34cc8155
Giovanni Morales <zS22013686@estudiantes.uv.mx>
2024-10-16 21:10:21 -0600

    Se creo el proyecto y configuro para poder desarrollar las primeras interfeces, al igual que se mapeo la base de datos y creo el host

---
+++ ServiciosCalabozosDragones/ICrearCuenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosCalabozosDragones
+{
+
+    [ServiceContract]
+    public interface ICrearCuenta
+    {
+        [OperationContract]
+        void AgregarCuenta(Cuenta cuenta);
+
+
+    }
+    [DataContract]
+    public class Cuenta
+    { 
+      
+        [DataMember]
+        public string Apodo { get; set; }
+        [DataMember]
+        public string Correo { get; set; }
+        [DataMember]
+        public string Contrasena{get; set; }
+    }
+}
---
+++ ServiciosCalabozosDragones/ImplementacionServicio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosCalabozosDragones
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class ImplementacionServicio : ICrearCuenta
+    {
+        public void AgregarCuenta(Cuenta cuenta)
+        {
+            Console.WriteLine("Usuario añadido" + cuenta.Apodo);
+        }
+
+        
+        
+    }
+}
